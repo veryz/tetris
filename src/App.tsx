ee8434eac4053b69e482a9c9fa647574f53695e1
mitsu <>
2025-04-19 03:47:22 +0200

    feat: allow to start/stop the game

--- src/App.tsx
+++ src/App.tsx
@@ -116,6 +116,8 @@ function App() {
           </ul>
 
           <button onClick={generateTetris}>create a tetris!</button>
+          <button onClick={() => tetris?.start()}>start</button>
+          <button onClick={() => tetris?.stop()}>stop</button>
         </div>
       </div>
     </>
